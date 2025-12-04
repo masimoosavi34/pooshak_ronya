e5e3735a6c7f1e784030e78c9ed782c9f61034f9
masimoosavi34 <masimoosavi34@gmail.com>
2025-12-04 18:25:30 +0330

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -52,11 +52,16 @@ post_save.connect(create_profile, sender=User)
 class Product(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=500, default='', blank=True, null=True)
-    price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
+    # قیمت‌ها به صورت عدد صحیح ذخیره می‌شوند
+    price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     picture = models.ImageField(upload_to='upload/product/')
     is_sale = models.BooleanField(default=False)
-    sale_price = models.DecimalField(default=0, decimal_places=0, max_digits=12)
+    sale_price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
 
     def __str__(self):
         return self.name
